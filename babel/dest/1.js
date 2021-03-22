f9397cb21c381898fb412ac4ac98379189301c81
J-Rainbow <1397194173@qq.com>
2021-03-22 15:02:53 +0800

    all project

---
+++ babel/dest/1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = 12;
+var b = 24;
+
+var ab = function ab(a, b) {
+  return a + b;
+};
+
+alert(ab(a, b));
---
+++ babel/src/1.js
@@ -0,0 +1,4 @@
+let a = 12
+let b = 24
+let ab = (a,b)=>a+b
+alert(ab(a,b))
