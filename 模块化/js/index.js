40b53659689c1edb0665ec21020a50cf2e8db62e
J-Rainbow <1397194173@qq.com>
2021-03-22 16:09:33 +0800

    模块化和面向对象

---
+++ 模块化/js/index.js
@@ -0,0 +1,2 @@
+import * as mod1 from './mod1' // '*'把所有的成员取出来，并且取名 mod1
+console.log(mod1.a,mod1.b,mod1.c)
---
+++ 模块化/webpack.config.js
@@ -0,0 +1,4 @@
+module.exports= {
+    mode: 'production',
+    entry: './js/index.js' //需要编译的js路径 指定js 防止全部编译
+}
